70abd3420cd6057c3f9aa491f5fa2f4a0de5534a
Hoan <hoanhihi123456@gmail.com>
2024-07-02 15:16:38 +0700

    push source code

---
+++ src/main/java/com/example/demo/Testcreate72Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testcreate72Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testcreate72Application.class, args);
+	}
+
+}
